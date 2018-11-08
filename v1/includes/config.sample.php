b443a7c73b251195a2279584ce5482ecfd696c3b
Luis Gonzalez <luisarmando1234@gmail.com>
2018-11-08 10:43:41 -0600

    Update config.sample.php

--- v1/includes/config.sample.php
+++ v1/includes/config.sample.php
@@ -1,5 +1,5 @@
 <?php
-define('CLIENT_ID','1056345468482-33n2j5cmlq69v3n0hhb5ieq0harjfi5e.apps.googleusercontent.com');
-define('CLIENT_SECRET','keZ91uDT5gG9p9aJhQwOisaW');
+define('CLIENT_ID','');
+define('CLIENT_SECRET','');
 define('APPLICATION_NAME','Inquid Email Reader');
-define('DEVELOPER_KEY','AIzaSyDTxX8D-Aon8sLwZWFot7-EeT9RUejWPzQ');
+define('DEVELOPER_KEY','');
